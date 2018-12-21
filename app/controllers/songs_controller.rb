7d7c10097858f9964601f55badfaa6842631dc9b
Mike Dilley <mike.dilley@gmail.com>
2018-12-21 15:08:46 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,6 +20,7 @@ class SongsController < ApplicationController
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
     @song.genre_ids = params[:genres]
     @song.save
+    flash[:message] = "Successfully created song."
     redirect "/songs/#{@song.slug}"
   end
 
@@ -32,6 +33,7 @@ class SongsController < ApplicationController
     @song = Song.find_by_slug(params[:slug])
     @song.artist.update(:name => params["Artist Name"])
     @song.genre_ids = params["genres"]
+    flash[:message] = "Successfully edited song."
     redirect "/songs/#{@song.slug}"
   end
 
