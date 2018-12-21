54db9d4b2db6c2c61541a8cf50612fc58d88e169
Mike Dilley <mike.dilley@gmail.com>
2018-12-21 14:34:33 +0000

    Automatically backed up by Learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,28 @@
+class SongsController < ApplicationController
+
+  get '/songs' do
+    @songs = Song.all
+    erb :"/songs/index"
+  end
+
+  get '/songs/new' do
+    erb :"/songs/new"
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :"/songs/show"
+  end
+
+  post '/songs' do
+    # binding.pry
+    @song = Song.create(:name => params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+    # Artist.find_by(:name => params["Artist Name"])
+
+    redirect "/songs/#{@song.slug}"
+  end
+
+end
