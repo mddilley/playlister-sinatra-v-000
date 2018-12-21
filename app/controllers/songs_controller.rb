7d1fae948c150f539e3ea1aaf135e00e92e0428b
Mike Dilley <mike.dilley@gmail.com>
2018-12-21 15:05:04 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -30,7 +30,7 @@ class SongsController < ApplicationController
 
   patch '/songs/:slug' do
     binding.pry
-    @song = Song.find_slug(params[:slug])
+    @song = Song.find_by_slug(params[:slug])
     @song.artist.update(:name => params["Artist Name"])
     @song.genre_ids = params["genres"]
     redirect "/songs/#{@song.slug}"
